57acdae4a2fbcb779bd3b96f13539995ea2ec036
catalystcodes <muh4mub@gmail.com>
2024-09-13 00:17:48 +0100

    feat: redux-persist

--- store.js
+++ store.js
@@ -1,8 +1,24 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import balanceReducer from "./balanceSlice";
 
+//Add persist configuration
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage: AsyncStorage,
+};
+
+//Adding RootReducer
+const rootReducer = combineReducers({
+  balance: balanceReducer,
+});
+
+// persisting our rootReducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// creating our store and exporting it
 export const store = configureStore({
-  reducer: {
-    balance: balanceReducer,
-  },
+  reducer: persistedReducer,
 });
